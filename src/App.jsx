ba549f0722ca29fc412023db4f6b3403c3e7772e
Jemson <jemsonmislani@gmail.com>
2026-04-14 12:32:53 +0800

    Do the header links, import font montserrat, sans-serif, style header links.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { HeaderSection } from './components/Header'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 export default function App() {
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HeaderSection />}/>
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 export function HeaderSection() {
     return(
         <>
-            
+            <div className='header-elements'>
+                <div className='header-links'>
+                    <Link className='home-link'>Home</Link>
+                    <Link className='projects-link'>Projects</Link>
+                    <Link className='skills-link'>Skills</Link>
+                    <Link className='contact-link'>Contact</Link>
+                </div>
+            </div>
         </>
     );
 }
