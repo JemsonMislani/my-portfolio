2eca1253d969fccb6a32bb798112fda1398ab93c
Jemson <jemsonmislani@gmail.com>
2026-04-14 13:16:10 +0800

    Create a section page and about me component, export and import it to main App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { HeaderSection } from './components/Header'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import { SectionPage } from './components/Section'
 export default function App() {
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HeaderSection />}/>
-        
+        <Route path='/home-page' element={<SectionPage/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/Section.jsx
@@ -0,0 +1,6 @@
+
+import './Section.css';
+export function SectionPage() {
+    <>
+    </>
+}
