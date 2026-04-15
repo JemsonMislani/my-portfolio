a98df8d8d3a7ccac5a6edb5c4e5aff08a8a1a4cc
Jemson <jemsonmislani@gmail.com>
2026-04-15 12:11:18 +0800

    Style Contact section.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,28 @@
+import { FiCopy } from 'react-icons/fi';
 import './Contact.css';
+import { useState } from 'react';
 export function ContactPage() {
+    const [setCopyEmail] = useState(false);
+    const myEmail = 'jemsonmislani@gmail.com';
+    
+    const copymyEmail = () => {
+        navigator.clipboard.writeText(myEmail);
+        setCopyEmail(true);
+        setTimeout(() => setCopyEmail(false, 1500));
+    }
+
     return(
         <>
+            <div className='contact-elements'>
+                <h1>Contact</h1>
+            </div>
+            <div className='contact-me'>
+                <p 
+                    className='contact-email'
+                    onClick={copymyEmail}>🖂: {myEmail} <FiCopy/>
+                    </p>
+                <p className='contact-cv'>CV: Resume Jemson</p>
+            </div>
         </>
     );
 }
