4588d35411aa1edbc3198f6267c51953079e4dfe
Jemson <jemsonmislani@gmail.com>
2026-04-15 11:32:25 +0800

    Create a footer component for Contact elements, create a file for contact page, export and import it to components.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import { HeaderSection } from './components/Header'
 import { SectionPage } from './components/Section'
+import { FooterPage } from './components/Footer'
 export default function App() {
 
   return (
     <BrowserRouter>
       <HeaderSection />
       <SectionPage />
+      <FooterPage />
     </BrowserRouter>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import { ContactPage  } from "../pages/Contact";
+export function FooterPage() {
+    return(
+        <>
+            {/* Contact page*/}
+            <ContactPage />
+        </>
+    );
+}
