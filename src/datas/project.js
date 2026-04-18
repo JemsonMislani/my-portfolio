de8daa2226698a7084a1e25c10ae9b34eb22d30b
Jemson <jemsonmislani@gmail.com>
2026-04-18 11:30:11 +0800

    Add data for descriptions of projects.

--- src/datas/project.js
+++ src/datas/project.js
@@ -3,19 +3,17 @@ export const projects = [{
     projectVideo: 'https://www.youtube.com/embed/SMKZButOyIs',
     projectImage: '../project-img-ecom.jpg',
     projectName: 'Step Matters',
-    projectDetails: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+    projectDetails: 'This project is a functional e-commerce website designed to provide real shopping experience.',
     description: [
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Built with a focus on responsive design, usability, and efficient navigation. Making it accessible on both desktop and mobile devices.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Features a product listings, a shopping cart, user authentication and a simple checkout.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Built using React, JS, Node.js for providing a smooth and interactive online shopping experience.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Uses local storage to save cart contents and buyer information, ensuring data persists across sessions.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
-
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.'
+        '- Backend development is currently underway to implement secure checkout, product management, and data storage, for making it fully functional.'
     ],
     stackUse: [
         'React',
@@ -33,19 +31,15 @@ export const projects = [{
     projectVideo: 'https://www.youtube.com/embed/67SksQoLSfI',
     projectImage: '../project-img-lyrics&chords.jpg',
     projectName: 'Lyrics & Chords library',
-    projectDetails: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+    projectDetails: 'Interactive music library for musicians to instantly search and get the songs, and view the lyrics and chords.',
     description: [
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Featured with auto-slider for a smooth and accessible experience, dark and light mode so users can pick what`s comfortable for their view.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Built with responsive design, provides an enjoyable experience on desktop and mobile devices.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Uses local storage to securely store user credentials for account creation and login.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
-
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
-
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.'
+        '- Future backend integration is planned to enable dynamic song management and enhance functionality.'
     ],
     stackUse: [
         'React',
@@ -63,19 +57,17 @@ export const projects = [{
     projectVideo: 'https://www.youtube.com/embed/CmD5SqDBhEE',
     projectImage: '../project-img-dailytask.png',
     projectName: 'Daily task manager',
-    projectDetails: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+    projectDetails: 'A project web-based task management tool that helps users organize and track their daily tasks.',
     description: [
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Users can add tasks by typing the task and selecting a due date, it provides interactive buttons to mark and see the progress of tasks.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- Offers a clear overview of task progress that helps users to stay organized.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- This Platform uses local storage to save user credentials and tasks, ensuring that data persists across sessions.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
+        '- With tasks auto-sorted by status and due date so completed task are neatly arranged.',
 
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.',
-
-        '- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat ullam veniam velit ut expedita dicta sint impedit officiis facilis accusantium.'
+        '- Future backend development is planned to enable database storage and full user account management.'
     ],
     stackUse: [
         'React',
